11e4308516cdb9f8b9158c327897feef2c9d4551
EricSoklowski <ericsoko2002@gmail.com>
2022-09-03 21:33:20 -0400

    add winning combos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
-
+winningCombos = [[sq0, sq1, sq2], [sq3, sq4, sq5], [sq6, sq7, sq8], [sq0, sq3, sq6], [sq1, sq4, sq7], [sq2, sq5, sq8], [sq0, sq4, sq8], [sq2, sq4, sq6]]
 
 
 
